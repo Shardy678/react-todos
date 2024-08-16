defa9e64bb1b95faaa567873435890661f3bfc93
Shardy678 <tambov.j678@gmail.com>
2024-08-16 12:57:53 +0300

    complete to-do list app

---
+++ src/TodoList.jsx
@@ -0,0 +1,66 @@
+import { useState, useEffect } from 'react';
+import List from '@mui/material/List';
+import TodoItem from './TodoItem';
+import TodoForm from './TodoForm';
+import { Box, Typography }from "@mui/material"
+
+const getInitialData = () => {
+    const data = JSON.parse(localStorage.getItem("todos"))
+    if(!data) return [];
+    return data
+}
+
+export default function TodoList() {
+    const [todos, setTodos] = useState(getInitialData);
+
+useEffect(()=>{
+    localStorage.setItem(
+        'todos',JSON.stringify(todos)
+    )
+},[todos])
+
+    const handleToggle = (id) => () => {
+        setTodos((prevTodos) =>
+            prevTodos.map((todo) =>
+                todo.id === id ? { ...todo, completed: !todo.completed } : todo
+            )
+        );
+    };
+
+    const removeTodo = (id) => {
+        setTodos(prevTodos => {
+            return prevTodos.filter(t => t.id !== id)
+        })
+    }
+
+    const addTodo = (text) => {
+        setTodos(prevTodos => {
+            return [...prevTodos,{text: text, id:crypto.randomUUID(),completed:false}]
+        })
+    }
+    return (
+        <Box sx={{
+            display:'flex',
+            justifyContent:'center',
+            flexDirection:'column',
+            alignItems:'center',
+            m:3,
+        }}>
+            <Typography variant="h2" component="h1" sx={{ flexGrow: 1 }}>
+            Todos
+            </Typography>
+            <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+            {todos.map((todo) => (
+                <TodoItem 
+                    key={todo.id}
+                    todo={todo}
+                    onToggle={handleToggle(todo.id)}
+                    removeTodo={() => removeTodo(todo.id)}
+                />
+            ))}
+            <TodoForm addTodo={addTodo}/>
+            </List>
+        </Box>
+        
+    );
+}
